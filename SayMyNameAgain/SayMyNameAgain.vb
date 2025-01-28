6a289cb20ee6936e5648152b329f24c67aeb661c
Jason Permann <JaceFoxman@gmail.com>
2025-01-28 09:07:06 -0700

    Add cases

--- SayMyNameAgain/SayMyNameAgain.vb
+++ SayMyNameAgain/SayMyNameAgain.vb
@@ -4,9 +4,33 @@
 'OperatorsExample
 'https://github.com/JaceFoxman/SayMyNameAgain.git
 
+Option Explicit On
+
+
 Module SayMyNameAgain
 
     Sub Main()
+
+        Dim userInput As String
+
+        Do
+            Console.Write("Please enter your name: ")
+            userInput = Console.Read()
+
+            Select Case userInput
+
+                Case userInput = "Emily"
+                    Console.WriteLine($"Welcome, {userInput}")
+                Case userInput = "Joe"
+                    Console.WriteLine($"Welcome, {userInput}")
+                Case userInput = "Jason"
+                    Console.WriteLine($"Welcome back Sir.")
+
+            End Select
+
+        Loop Until = "Q"
+
+        Console.WriteLine($"have a nice day, {userInput}")
 
     End Sub
 
